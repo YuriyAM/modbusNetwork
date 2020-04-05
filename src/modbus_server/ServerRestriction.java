afd6ea2e51fd10dd62e5153370e0fbb3f976af56
YMyronov <mesda4@gmail.com>
2020-04-05 16:58:19 +0300

    Initial port setup warning was added

--- src/modbus_server/ServerRestriction.java
+++ src/modbus_server/ServerRestriction.java
@@ -7,7 +7,11 @@ public class ServerRestriction {
 
     private static int port;
 
-    // Gets random free port
+    /*
+     * Finds free port by creating a new socket with automatically allocated port.
+     * Opening and closing sockets is not a good practice so try not to use this
+     * method and set static port in range 49152...65535
+     */
     public static int getPort() {
         try {
             ServerSocket s = new ServerSocket(0);
